97501252d0f98b2c74c1d75eb231fe50c18f0909
Udo Urbantschitsch <udo@cloudwerkstatt.com>
2016-10-25 20:15:55 +0200

    Added python kubernetes discovery

--- container-files/docker-entrypoint.sh
+++ container-files/docker-entrypoint.sh
@@ -7,16 +7,23 @@ if [ ! -z $OPENSHIFT_BUILD_NAMESPACE ] && [ -f /var/run/secrets/kubernetes.io/se
   echo "----------------------------------- OpenShift - API -----------------------------------"
   echo "---------------------------------------------------------------------------------------"
 
-  #eps=$(/pod_endpoints.rb)
 
-  IP=$(ip addr show eth0 | grep "inet " | awk '{split($0,a," "); print a[2]}')
-  IP=${IP:0:-3}
+  TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
+  POD_COUNT=$(curl -s -k -H "Authorization: Bearer $TOKEN" https://kubernetes.default/api/v1/namespaces/$OPENSHIFT_BUILD_NAMESPACE/pods?labelSelector=app%3Delastic | python -c 'import json,sys;obj=json.load(sys.stdin);print len(obj["items"]);')
+  echo "Found $POD_COUNT elasticsearch nodes"
+  IPS="["
+  for ((i=0; i<$POD_COUNT; i++)); do
+    ip=$(curl -s -k -H "Authorization: Bearer $TOKEN" https://kubernetes.default/api/v1/namespaces/elastic/pods?labelSelector=app%3Delastic | python -c "import json,sys;obj=json.load(sys.stdin);print obj[\"items\"][$i][\"status\"][\"podIP\"];")
+    IPS+="\"$ip\", "
+  done
+  IPS=${IPS:0:-2}]
+  echo $IPS
 
   elasticsearch -Des.node.master=$node_master \
       -Des.node.data=$node_data \
       -Des.http.enabled=$http_enabled \
-  #    -Des.discovery.zen.ping.unicast.hosts=$eps \
-      --network.host $IP
+      -Des.discovery.zen.ping.unicast.hosts=$IPS
+
 else
   # Not on OpenShift -> Run in Multicast Mode
   echo "----------------------------------- Multicast Mode -----------------------------------"
